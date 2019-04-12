b49ec0523552f727e8d787adb2ee38afe86c471a
Brian Hoffman <brianjhoffman@gmail.com>
2019-04-12 13:06:39 -0400

    refactor to use PKP policy system

--- SwordServerPlugin.inc.php
+++ SwordServerPlugin.inc.php
@@ -18,11 +18,29 @@ import('classes.journal.SectionDAO');
 import('classes.article.ArticleDAO');
 require __DIR__ . '/ServiceDocument.inc.php';
 require __DIR__ . '/DepositReceipt.inc.php';
-require __DIR__ . '/SwordError.inc.php';
 require __DIR__ . '/SwordSubmissionFileManager.inc.php';
-
+require __DIR__ . '/SwordServerAccessPolicy.inc.php';
 
 class SwordServerPlugin extends GatewayPlugin {
+
+
+	function __construct() {
+		parent::__construct();
+
+		$this->_endpoints = [
+			[
+				'pattern' => 'servicedocument',
+				'method' => 'GET',
+				'handler' => [$this, 'serviceDocument']
+			],
+			[
+				'pattern' => 'sections/{id}',
+				'method' => 'POST',
+				'handler' => [$this, 'deposit']
+			]
+		];
+	}
+
 	/**
 	 * @copydoc Plugin::register()
 	 */
@@ -58,160 +76,150 @@ class SwordServerPlugin extends GatewayPlugin {
 		return __('plugins.gateways.swordserver.description');
 	}
 
+	function addPolicies($request) {
+		yield new SwordServerAccessPolicy($request, );
+	}
+
+	function serviceDocument() {
+		$journal = $this->request->getJournal();
+		$journalId = $journal->getId();
+		$sectionDAO = new SectionDAO();
+		$resultSet = $sectionDAO->getByJournalId($journalId);
+		$sections = $resultSet->toAssociativeArray();
+		$serviceDocument = new ServiceDocument(
+			$journal,
+			$sections,
+			$this->request->getRequestUrl()
+		);
+		header('Content-Type: application/xml');
+		echo $serviceDocument->saveXML();
+		exit;
+	}
+
+	function deposit($args) {
+		$headers = getallheaders();
+		$sectionId = intval(array_shift($args));
+		$submissionDao = Application::getSubmissionDAO();
+		$submission = $submissionDao->newDataObject();
+		$submission->setContextId($this->request->getJournal()->getId());
+		$submission->setDateSubmitted (Core::getCurrentDate());
+		$submission->setLastModified (Core::getCurrentDate());
+		$submission->setSubmissionProgress(0);
+		$submission->setStageId(WORKFLOW_STAGE_ID_SUBMISSION);
+		$submission->setTitle("SWORD TEST DEPOSIT " . time(), 'en_US');
+		$submission->setSectionId($sectionId);
+		$submission->setLocale('en_US');
+
+		$uploadDir = ini_get('upload_tmp_dir');
+		$uploadedFilePath = $uploadDir . '/sword_upload.dat';
+		file_put_contents($uploadedFilePath, file_get_contents('php://input'));
+		if ($headers['Content-Type'] == 'application/zip' && $headers['Packaging'] == "http://purl.org/net/sword/package/METSDSpaceSIP") {
+			$zip = new ZipArchive;
+			$res = $zip->open($uploadedFilePath);
+			if ($res) {
+				$zip->extractTo($uploadDir);
+				$zip->close();
+			} else {
+				throw new Exception('Zip extraction failed');
+			}
+		}
+
+		if (!file_exists($uploadDir . "/mets.xml")) {
+			throw new Exception('No mets.xml document in extracted Zip package');
+		}
+		$metsDoc = simplexml_load_file($uploadDir . "/mets.xml");
+		$metsDoc->registerXPathNamespace("mets", 'http://www.loc.gov/METS/');
+		$metsDoc->registerXPathNamespace("epdcx", "http://purl.org/eprint/epdcx/2006-11-16/");
+		$match = $metsDoc->xpath("//epdcx:statement[@epdcx:propertyURI='http://purl.org/dc/" .
+								 "elements/1.1/title']/epdcx:valueString");
+		if (!empty($match)) {
+			$title = $match[0]->__toString();
+			$submission->setTitle($title, 'en_US');
+		}
+
+		$submissionId = $submissionDao->insertObject($submission);
+
+		$hrefs = array_unique(
+			array_map(function($h) {
+				return $h['href']->__toString();
+			}, $metsDoc->xpath("//mets:FLocat[@LOCTYPE='URL']/@xlink:href")
+			)
+		);
+		foreach ($hrefs as $href) {
+			if (file_exists($uploadDir . "/" . $href)) {
+				$submissionFileManager = new SwordSubmissionFileManager($this->request->getJournal()->getId(), $submissionId);
+				$submissionFile = $submissionFileManager->uploadSubmissionFile(
+					$href, 2, 1, null, 11, null, null
+				);
+			}
+		}
+
+		// Attach the original package as well
+		$submissionFileManager = new SwordSubmissionFileManager($this->request->getJournal()->getId(), $submissionId);
+		$submissionFile = $submissionFileManager->uploadSubmissionFile(
+			'sword_upload.dat', 2, 1, null, 11, null, null
+		);
+
+		$serverHost = $this->request->_serverHost;
+		$requestPath = $this->request->_requestPath;
+		$editIri = 'http://' . $serverHost .
+				 substr($requestPath, 0, strrpos($requestPath, "sections")) .
+				 'submissions/' . $submissionId;
+
+		$depositReceipt = new DepositReceipt(
+			[
+				'title' => $submission->getTitle('en_US'),
+				'edit-iri' => $editIri
+			]
+		);
+		header('Content-Type: application/xml');
+		echo $depositReceipt->saveXML();
+		exit;
+	}
+
+
 	/**
 	 * Handle fetch requests for this plugin.
 	 */
 	function fetch($args, $request) {
-
 		if (!$this->getEnabled()) {
 			return false;
 		}
+		$this->request = $request;
 
-		switch (array_shift($args)) {
-		case 'servicedocument':
-			if (!$request->isGet()) {
-				return false;
-			}
-			$journal = $request->getJournal();
-			$journalId = $journal->getId();
-			$sectionDAO = new SectionDAO();
-			$resultSet = $sectionDAO->getByJournalId($journalId);
-			$sections = $resultSet->toAssociativeArray();
-			$serviceDocument = new ServiceDocument(
-				$journal,
-				$sections,
-				$request->getRequestUrl()
-			);
-			header('Content-Type: application/xml');
-			echo $serviceDocument->saveXML();
-			exit;
-		case 'sections':
-			if (!$request->isPost()) {
-				return false;
-			}
-
-			$headers = getallheaders();
-			if (!array_key_exists('Authorization', $headers)) {
-				$this->errorResponse(401, "No Authorization header sent with request");
-			}
-
-			$auth_header = $headers["Authorization"];
-			$userPass = base64_decode(substr($auth_header, 6));
-			$userPass = explode(":", $userPass);
-
-			if (!Validation::checkCredentials($userPass[0], $userPass[1])) {
-				$this->errorResponse(401, "Unauthorized");
-			}
-
-			$userDao = new UserDao();
-			$user = $userDao->getByUsername($userPass[0]);
-			if (! $user->hasRole(ROLE_ID_MANAGER, $request->getJournal()->getId())) {
-				$this->errorResponse(401, "Unauthorized");
-			}
-
-			$headers = getallheaders();
-			$sectionId = intval(array_shift($args));
-			$submissionDao = Application::getSubmissionDAO();
-			$submission = $submissionDao->newDataObject();
-			$submission->setContextId($request->getJournal()->getId());
-			$submission->setDateSubmitted (Core::getCurrentDate());
-			$submission->setLastModified (Core::getCurrentDate());
-			$submission->setSubmissionProgress(0);
-			$submission->setStageId(WORKFLOW_STAGE_ID_SUBMISSION);
-			$submission->setTitle("SWORD TEST DEPOSIT " . time(), 'en_US');
-			$submission->setSectionId($sectionId);
-			$submission->setLocale('en_US');
-
-			$uploadDir = ini_get('upload_tmp_dir');
-			$uploadedFilePath = $uploadDir . '/sword_upload.dat';
-			file_put_contents($uploadedFilePath, file_get_contents('php://input'));
-			if ($headers['Content-Type'] == 'application/zip' && $headers['Packaging'] == "http://purl.org/net/sword/package/METSDSpaceSIP") {
-				$zip = new ZipArchive;
-				$res = $zip->open($uploadedFilePath);
-				if ($res) {
-					$zip->extractTo($uploadDir);
-					$zip->close();
-				} else {
-					throw new Exception('Zip extraction failed');
+		$route = (array_shift($args));
+		$method = $request->getRequestMethod();
+		$methodsAllowed = [];
+		foreach ($this->_endpoints as $endpoint) {
+			if (substr($endpoint['pattern'], 0, strlen($route)) == $route) {
+				if ($method == $endpoint['method']) {
+					call_user_func($endpoint['handler'], $args);
+					break;
 				}
+				array_push($methodsAllowed, $endpoint['method']);
 			}
-
-			if (!file_exists($uploadDir . "/mets.xml")) {
-				throw new Exception('No mets.xml document in extracted Zip package');
-			}
-			$metsDoc = simplexml_load_file($uploadDir . "/mets.xml");
-			$metsDoc->registerXPathNamespace("mets", 'http://www.loc.gov/METS/');
-			$metsDoc->registerXPathNamespace("epdcx", "http://purl.org/eprint/epdcx/2006-11-16/");
-			$match = $metsDoc->xpath("//epdcx:statement[@epdcx:propertyURI='http://purl.org/dc/" .
-															 "elements/1.1/title']/epdcx:valueString");
-			if (!empty($match)) {
-				$title = $match[0]->__toString();
-				$submission->setTitle($title, 'en_US');
-			}
-
-			$submissionId = $submissionDao->insertObject($submission);
-
-			$hrefs = array_unique(
-				array_map(function($h) {
-					return $h['href']->__toString();
-				}, $metsDoc->xpath("//mets:FLocat[@LOCTYPE='URL']/@xlink:href")
-				)
-			);
-			foreach ($hrefs as $href) {
-				if (file_exists($uploadDir . "/" . $href)) {
-					$submissionFileManager = new SwordSubmissionFileManager($request->getJournal()->getId(), $submissionId);
-					$submissionFile = $submissionFileManager->uploadSubmissionFile(
-						$href, 2, 1, null, 11, null, null
-					);
-				}
-			}
-
-			// Attach the original package as well
-			$submissionFileManager = new SwordSubmissionFileManager($request->getJournal()->getId(), $submissionId);
-			$submissionFile = $submissionFileManager->uploadSubmissionFile(
-				'sword_upload.dat', 2, 1, null, 11, null, null
-			);
-
-			$serverHost = $request->_serverHost;
-			$requestPath = $request->_requestPath;
-			$editIri = 'http://' . $serverHost .
-					 substr($requestPath, 0, strrpos($requestPath, "sections")) .
-					 'submissions/' . $submissionId;
-
-			$depositReceipt = new DepositReceipt(
-				[
-					'title' => $submission->getTitle('en_US'),
-					'edit-iri' => $editIri
-				]
-			);
-			header('Content-Type: application/xml');
-			echo $depositReceipt->saveXML();
-			exit;
 		}
 
-		// Failure.
-		header('HTTP/1.0 404 Not Found');
-		$templateMgr = TemplateManager::getManager($request);
-		AppLocale::requireComponents(LOCALE_COMPONENT_APP_COMMON);
-		$templateMgr->assign('message', 'plugins.gateways.swordserver.errors.errorMessage');
-		$templateMgr->display('frontend/pages/message.tpl');
-		exit;
-	}
+		if (!empty($methodsAllowed)) {
+			$swordError = new SwordError([
+				'summary' => "Method not Allowed."
+			]);
 
-	function errorResponse($httpCode, $errorSummary, $errorType = null) {
-		$swordError = new SwordError([
-			'summary' => $errorSummary
-		]);
-		header('Content-Type: application/xml');
+			header('Content-Type: application/xml');
+			header('HTTP/1.1 405 Not Allowed');
+			header('Allow ' . implode($methodsAllowed, ', '));
+			echo $swordError->saveXML();
+			exit;
+		} else {
+			$swordError = new SwordError([
+				'summary' => "Not found."
+			]);
 
-		switch ($httpCode) {
-		case 401:
-			header("HTTP/1.1 401 Unauthorized");
-			break;
-		default:
-			header("HTTP/1.1 500 Server Error");
+			header('Content-Type: application/xml');
+			header('HTTP/1.1 404 Not Found');
+			echo $swordError->saveXML();
+			exit;
 		}
-		echo $swordError->saveXML();
-		exit;
 	}
+
 }
