985dd4967b8e5653fa42e44ce173345a865f7a02
Jhon <jhon@lepidus.com.br>
2026-09-11 10:51:31 -0400

    Fixes API key authentication
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- SwordServerAccessPolicy.php
+++ SwordServerAccessPolicy.php
@@ -66,7 +66,7 @@ class SwordServerAccessPolicy extends AuthorizationPolicy {
 		}
 		// 2. Try API Key
 		if (!$user && $apiToken = ($headers['X-Ojs-Sword-Api-Token'] ?? null)) {
-				$secret = Config::getVar('security', 'api_key_secret', '');
+			$secret = \Config::getVar('security', 'api_key_secret', '');
 			try {
 				$decoded = JWT::decode($apiToken, $secret, ['HS256']);
 				// Compatibility with old API keys
@@ -74,8 +74,7 @@ class SwordServerAccessPolicy extends AuthorizationPolicy {
 				if (substr($decoded, 0, 2) === '""') {
 					$decoded = json_decode($decoded);
 				}
-				$userDao = DAORegistry::getDAO('UserDAO');
-				$user = $userDao->getBySetting('apiKey', $decoded);
+				$user = Repo::user()->getByApiKey($decoded);
 				Registry::set('user', $user);
 			} catch (Firebase\JWT\SignatureInvalidException $e) {
 			} catch (DomainException $e) {
