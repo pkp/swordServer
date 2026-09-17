83fb839eb468928b6f07324afed87d9d83ff2670
Jhonathan Miranda <60978081+JhonathanLepidus@users.noreply.github.com>
2026-09-17 15:01:42 -0400

    Fixes API Key authentication in 3.4.0 (#6)
    * Fixes API key authentication
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>
    
    * Removes unused import in class
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>
    
    ---------
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- SwordServerAccessPolicy.php
+++ SwordServerAccessPolicy.php
@@ -16,7 +16,6 @@ use \Firebase\JWT\JWT;
 
 use PKP\security\authorization\AuthorizationPolicy;
 use PKP\security\Validation;
-use PKP\db\DAORegistry;
 use PKP\core\Registry;
 use PKP\security\Role;
 
@@ -66,7 +65,7 @@ class SwordServerAccessPolicy extends AuthorizationPolicy {
 		}
 		// 2. Try API Key
 		if (!$user && $apiToken = ($headers['X-Ojs-Sword-Api-Token'] ?? null)) {
-				$secret = Config::getVar('security', 'api_key_secret', '');
+			$secret = \Config::getVar('security', 'api_key_secret', '');
 			try {
 				$decoded = JWT::decode($apiToken, $secret, ['HS256']);
 				// Compatibility with old API keys
@@ -74,8 +73,7 @@ class SwordServerAccessPolicy extends AuthorizationPolicy {
 				if (substr($decoded, 0, 2) === '""') {
 					$decoded = json_decode($decoded);
 				}
-				$userDao = DAORegistry::getDAO('UserDAO');
-				$user = $userDao->getBySetting('apiKey', $decoded);
+				$user = Repo::user()->getByApiKey($decoded);
 				Registry::set('user', $user);
 			} catch (Firebase\JWT\SignatureInvalidException $e) {
 			} catch (DomainException $e) {
